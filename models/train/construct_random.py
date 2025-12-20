85d2468e31749f4e4e7332fbdb05951a5a93f0ce
Aki <Stephanie.fermil@gmail.com>
2025-12-20 15:52:28 +0800

    Updated random outfit construction

--- models/train/construct_random.py
+++ models/train/construct_random.py
@@ -1,6 +1,6 @@
 import json
 import torch
-from models.train.model_train import num_classes, attr_dim
+import os
 
 def load_outfits(json_path):
     with open(json_path, "r") as f:
@@ -10,22 +10,36 @@ disjoint_train = load_outfits(
     "data/polyvore-outfit-dataset/polyvore_outfits/disjoint/train.json"
 )
 
-item_features = {
-    item_id: {
-        "clip": torch.Tensor([512]),
-        "category_onehot": torch.Tensor([num_classes]),
-        "attributes": torch.Tensor([attr_dim]),
-    }
-}
+data = torch.load("embeddings/polyvore_clip_embeddings.pt")
 
-item_features = torch.load("embeddings/polyvore_clip_embeddings.pt")
+image_paths = data["image_paths"]
+image_embeddings = data["image_embeddings"]
+
+item_features = {}
+
+for path, emb in zip(image_paths, image_embeddings):
+    item_id = os.path.splitext(os.path.basename(path))[0]
+
+    item_features[item_id] = {
+        "clip": emb,
+        # placeholders for now (we’ll fill later)
+        "category_onehot": None,
+        "attributes": None,
+    }
 
 def construct_outfit_from_json(outfit, item_features):
-    features = []
+    outfit_items = []
 
     for item in outfit["items"]:
         item_id = item["item_id"]
-        if item_id in item_features:
-            features.append(item_features[item_id])
 
-    return features
+        if item_id in item_features:
+            outfit_items.append(item_features[item_id])
+
+    return outfit_items
+
+outfit = disjoint_train[0]
+outfit_items = construct_outfit_from_json(outfit, item_features)
+
+print(len(outfit_items))
+print(outfit_items[0]["clip"].shape)
